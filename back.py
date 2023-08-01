065f90a8f5b9636296ea990aa676b7822a07e1d8
Bakhredin <bax2002mailru26@gmail.com>
2023-08-01 12:23:08 +0600

    new back

--- back.py
+++ back.py
@@ -12,7 +12,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-                   "https://epitet.vercel.app"
+                   "http://epitet.vercel.app"
                    ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
